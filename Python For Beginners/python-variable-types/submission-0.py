c05175e68dee0ccd2d732f7728f52c3956e051f5
ravina-panaparambil <110417557+ravina-panaparambil@users.noreply.github.com>
2026-08-12 05:38:54 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 1
+float_type = 98.2
+bool_type = False
+string_type = "Alice"
+list_type = [1,2,3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
