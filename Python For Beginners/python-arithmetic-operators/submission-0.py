77d88432fa6785c49f0a3295bc512f3965c32d38
ravina-panaparambil <110417557+ravina-panaparambil@users.noreply.github.com>
2026-08-12 05:46:53 -0700

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,12 @@
+a, b, c = 2, 2, 0.5
+
+sum_abc = a + b + c
+zero_minus_sum = 0 - sum_abc
+
+prod_abc = a * b * c
+div_sum_prod = sum_abc / prod_abc
+
+print(sum_abc)
+print(zero_minus_sum)
+print(prod_abc)
+print(div_sum_prod)
