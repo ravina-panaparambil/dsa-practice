a770d0deb93600e78c34a275e763b42e264b8311
ravina-panaparambil <110417557+ravina-panaparambil@users.noreply.github.com>
2026-08-12 05:40:04 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 3
+print(type(variable))
+variable = 3.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "ALice"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
