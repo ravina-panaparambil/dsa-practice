c97f96d5e8c211e7c36f5883ee3fa1dc3b39b755
ravina-panaparambil <110417557+ravina-panaparambil@users.noreply.github.com>
2026-08-27 18:18:57 -0700

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    user_input = [int(x) for x in input().split(',')]
+
+    return sum(user_input)
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
